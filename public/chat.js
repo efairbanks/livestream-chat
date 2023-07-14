1bef65da21e5546ecb1b06d64278d0103a6d30b6
Wren <OtherwiseJunk@gmail.com>
2023-07-13 23:50:04 -0400

    Add userCount display to show number of connected socket clients

--- public/chat.js
+++ public/chat.js
@@ -1,82 +1,81 @@
 // establish connection
-const socket = io.connect('/');
+const socket = io.connect("/");
 
 // query DOM
-const message = document.getElementById('message');
-const username = document.getElementById('username');
-const btn = document.getElementById('send');
-const output = document.getElementById('output');
-const feedback = document.getElementById('feedback');
-const chatContainer = document.getElementById('chat-window');
-const playerContainer = document.getElementById('player-container');
+const message = document.getElementById("message");
+const username = document.getElementById("username");
+const btn = document.getElementById("send");
+const output = document.getElementById("output");
+const feedback = document.getElementById("feedback");
+const chatContainer = document.getElementById("chat-window");
+const playerContainer = document.getElementById("player-container");
 
-const streamKey = window.location.href.slice(8).split('/')[1];
+const streamKey = window.location.href.slice(8).split("/")[1];
 const wasRecentlyTypingByUsername = {};
+let userCount = 1;
 
-fetch('/config?page=chat')
-  .then(async (data) => {
-    const config = await data.json();
-    appendVideoSourceToPlayerDiv(config.STREAM_HOST, streamKey);
-    var player = videojs('#player', { autoplay: true });
-    document.title = config.CHAT_TITLE ?? 'Movie Lobby';
-  });
+fetch("/config?page=chat").then(async (data) => {
+  const config = await data.json();
+  appendVideoSourceToPlayerDiv(config.STREAM_HOST, streamKey);
+  var player = videojs("#player", { autoplay: true });
+  document.title = config.CHAT_TITLE ?? "Movie Lobby";
+});
 
 function appendVideoSourceToPlayerDiv(host, streamKey) {
   playerContainer.innerHTML = `
   <video id="player" class="video-js vjs-default-skin" controls preload="auto">
     <source src="${host}/hls/${streamKey}.m3u8" type="application/x-mpegURL" />
   </video>
-  `
+  `;
 }
 
 function sendMessage() {
-  socket.emit('chat', {
+  socket.emit("chat", {
     stream: streamKey,
     message: message.value,
     username: username.value,
     createdAt: new Date(),
-    id: new Date().getTime()
+    id: new Date().getTime(),
   });
-  message.value = '';
+  message.value = "";
 }
 
 function clearIsTyping(isTypingElement, username) {
   if (!wasRecentlyTypingByUsername[username]) {
-    feedback.innerHTML = feedback.innerHTML.replace(isTypingElement, '')
-  }
-  else {
+    feedback.innerHTML = feedback.innerHTML.replace(isTypingElement, "");
+  } else {
     setTimeout(() => {
-      clearIsTyping(isTypingElement, username)
+      clearIsTyping(isTypingElement, username);
       wasRecentlyTypingByUsername[username] = false;
     }, 2 * 1000);
   }
 }
 
 // emit events
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
   sendMessage();
 });
 
-message.addEventListener('keypress', (e) => {
-  if (e.key == 'Enter') {
+message.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
     sendMessage();
   } else {
-    socket.emit('typing', {
+    socket.emit("typing", {
       username: username.value,
-      stream: streamKey
+      stream: streamKey,
     });
   }
 });
 
 // listen for events
-socket.on('chat', (data) => {
+socket.on("chat", (data) => {
   if (data.stream === streamKey) {
     appendMessage(data);
     chatContainer.scrollTop = chatContainer.scrollHeight;
   }
 });
 
-socket.on('typing', (typingEvent) => {
+socket.on("typing", (typingEvent) => {
   if (typingEvent.stream === streamKey) {
     const isTypingElement = `<p><em>${typingEvent.username} is typing a message...</em></p>`;
 
@@ -87,22 +86,37 @@ socket.on('typing', (typingEvent) => {
       setTimeout(() => {
         clearIsTyping(isTypingElement, typingEvent);
       }, 5 * 1000);
-    }
-    else {
+    } else {
       wasRecentlyTypingByUsername[typingEvent] = true;
     }
   }
 });
+socket.on("userJoined", (count) => {
+  console.log(`Received count value of ${count}, previous was ${userCount}`);
+  userCount = count;
+  updateViewerCount(userCount);
+  console.log(`User Joined! current user count: ${userCount}`);
+});
+socket.on("userLeft", (count) => {
+  console.log(`Received count value of ${count}, previous was ${userCount}`);
+  userCount = count;
+  updateViewerCount(userCount);
+  console.log(`User Left! current user count: ${userCount}`);
+});
 
 // load message history
-fetch(`/messages?streamKey=${streamKey}`)
-  .then(async (data) => {
-    const messages = await data.json();
-    for (const message of messages)
-      appendMessage(message);
-    chatContainer.scrollTop = chatContainer.scrollHeight;
-  })
+fetch(`/messages?streamKey=${streamKey}`).then(async (data) => {
+  const messages = await data.json();
+  for (const message of messages) appendMessage(message);
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+});
 
 function appendMessage(data) {
   output.innerHTML += extremelyLongMessagePreview(data);
+}
+
+function updateViewerCount(userCount) {
+  const viewCountDisplay = document.getElementById("viewer-count-display");
+  const friendString = userCount == 1 ? "friend" : "friends";
+  viewCountDisplay.textContent = ` ${userCount} ${friendString} watching!`;
 }
--- websocket.js
+++ websocket.js
@@ -1,37 +1,49 @@
-const { messages, server } = require('./server');
-const socket = require('socket.io');
-const got = require('got');
-const { toHTML } = require('discord-markdown');
-const { textEmoji } = require('markdown-to-text-emoji'); 
+const { messages, server } = require("./server");
+const socket = require("socket.io");
+const got = require("got");
+const { toHTML } = require("discord-markdown");
+const { textEmoji } = require("markdown-to-text-emoji");
 
-const metascraper = require('metascraper')([
-  require('metascraper-description')(),
-  require('metascraper-image')(),
-  require('metascraper-title')(),
-  require('metascraper-url')()
+const metascraper = require("metascraper")([
+  require("metascraper-description")(),
+  require("metascraper-image")(),
+  require("metascraper-title")(),
+  require("metascraper-url")(),
 ]);
 
 const io = socket(server);
+let userCount = 0;
 
-io.on('connection', (clientSocket) => {
-  console.log('Made socket connection', clientSocket.id);
+io.on("connection", (clientSocket) => {
+  userCount++;
+  console.log("Made socket connection", clientSocket.id);
+  clientSocket.emit("userJoined", userCount);
+  clientSocket.broadcast.emit("userJoined", userCount);
 
-  clientSocket.on('chat', async (data) => {
+  clientSocket.on("chat", async (data) => {
     await setEmbed(data);
 
     data.html = toHTML(textEmoji(data.message));
 
-    if(messages[data.stream] == null){
-      messages[data.stream] = new Array
+    if (messages[data.stream] == null) {
+      messages[data.stream] = new Array();
     }
     const newIndex = messages[data.stream].push(data) - 1;
     setTimeout(() => messages[data.stream].splice(newIndex, 1), 5 * 60 * 1000);
-    
-    io.sockets.emit('chat', data);
+
+    io.sockets.emit("chat", data);
   });
 
-  clientSocket.on('typing', (data) => {
-    clientSocket.broadcast.emit('typing', data);
+  clientSocket.on("typing", (data) => {
+    clientSocket.broadcast.emit("typing", data);
+  });
+
+  clientSocket.on("disconnect", () => {
+    userCount--;
+
+    if (userCount < 0) userCount = 0;
+
+    clientSocket.broadcast.emit("userLeft", userCount);
   });
 });
 
